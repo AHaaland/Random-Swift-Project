2d086adb82e1168183144dbfbe21b2198f552b57
Andrew Haaland <dhaaland@hvc.rr.com>
2017-05-24 19:56:41 -0400

    First test using iOS apps

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var myLabel: UILabel!
+	@IBOutlet weak var newLabel: UILabel!
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var nameTxtBox: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		nameTxtBox.delegate = self
     }
+	@IBAction func helloBTN(_ sender: AnyObject) {
+		let name = nameTxtBox.text!
+		nameLabel.text = "Hello, \(name)!"
+		nameTxtBox.resignFirstResponder()
+		//oposite is becomeFirstResponder()
+	}
+	//other way to say done editing text -> shuts KB, when touch anywhere else
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		view.endEditing(true)
+	}
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		nameTxtBox.resignFirstResponder()
+		return false;
+	}
+	
+	@IBAction func didTouchButton(_ sender: AnyObject) {
+		myLabel.text = "Changed with Code!"
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
