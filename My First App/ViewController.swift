d74e91000f5d05ca1e351e7e7e167519cc70a052
Andrew Haaland <dhaaland@hvc.rr.com>
2017-05-24 18:29:34 -0400

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Andrew Haaland on 5/24/17.
+//  Copyright © 2017 Andrew Haaland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
